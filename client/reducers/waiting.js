1ce47975cab24a383b576ef44c2b7d819d7f9a98
Jasmine Sinclair <jasminersinclair@gmail.com>
2022-05-05 17:48:29 +1200

    add action creator for wait indicator

--- client/reducers/waiting.js
+++ client/reducers/waiting.js
@@ -1,11 +1,10 @@
-// TODO: import actions here, replace hardcoded "SET_WAITING"/"UNSET_WAITING" cases
-// eg CREATING_PROFILE
+import { SET_WAITING, UNSET_WAITING } from '../actions/waiting'
 
 const reducer = (state = false, action) => {
   switch (action.type) {
-    case "SET_WAITING":
+    case SET_WAITING:
       return true
-    case "UNSET_WAITING":
+    case UNSET_WAITING:
       return false  
     default:
       return state  
