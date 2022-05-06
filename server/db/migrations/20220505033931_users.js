7e5ef2bfe798921e25f8edbbdec21e3085d6a92c
HobbesWSL <jessewilson@outlook.com>
2022-05-06 13:56:21 +1200

    make email non unique

--- server/db/migrations/20220505033931_users.js
+++ server/db/migrations/20220505033931_users.js
@@ -6,7 +6,7 @@ exports.up = async (knex) => {
     table.string('first_name').notNullable()
     table.string('last_name')
     table.string('username').notNullable().unique()
-    table.string('email').notNullable().unique()
+    table.string('email').notNullable()
     table.string('about').notNullable()
     table
       .enu('status', ['active', 'inactive'])
