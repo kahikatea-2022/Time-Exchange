0b474a090353839574b75ba51a33a339c8cf4781
Matt Radich <mattradich24@gmail.com>
2022-05-06 16:29:22 +1200

    header-nav-working-adjusted-cursor

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Nav from './Nav/Nav'
+import { Link } from 'react-router-dom'
 
 function Header() {
       return (
@@ -7,7 +8,7 @@ function Header() {
       <>
       <div className='header-container'>
         
-        <h1 className="logo">TIMEEXCHANGE</h1>
+        <a href='/'><h1 className="header-title">TIMEEXCHANGE</h1></a>
         
       </div>
       <Nav />
