0240d0250448af0d47dd08896fa036dd93cb7011
Matt Radich <mattradich24@gmail.com>
2022-05-07 12:20:40 +1200

    home-page-wip

--- client/pages/Home.jsx
+++ client/pages/Home.jsx
@@ -2,7 +2,43 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>I AM HOME</div>
+    <div className='page-container'>
+      <div className='main-content-container'>
+        <div className='slogan-image-container'>
+          <div className='slogan-container'>
+            <h1 id='slogan'><i>everybody is a expert <br></br>at something.</i></h1>
+          </div>
+          <div className='image-container'>
+            <img src='/hobby-icons.jpg' id='image'></img>
+          </div>
+        </div>
+        <div className='app-blurb-container'>
+            <div className='blurb-text'>
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing e
+                lit. Modi eum corrupti ipsum. Voluptatibus aliquid l
+                aboriosam consequatur quisquam non optio placeat acc
+                usantium et veniam natus culpa modi quis recusandae 
+                nemo sed deleniti ipsum nostrum animi necessitatibus
+                , odit nisi? Neque iure consequuntur fuga ducimus ape
+                riam accusantium, est, suscipit deserunt cum exercita
+                tionem atque quae magnam at eveniet. Vitae consequatu
+                r repellendus vero delectus ipsa laboriosam modi sol
+                uta quisquam dolorem eius quas cumque, nemo quos qui
+                
+              </p>
+            </div>
+        </div>
+        <div className='reviews-container'>
+          <div>Box 1 Lorem ipsum, dol
+          or sit amet consectetur adipisicing elit. Odio max
+          ime commodi eligendi, modi dolore soluta rerum ad dicta. 
+          Repellat, neque ad cum amet hic rerum
+          . Quod expedita pariatur libero placeat?</div>
+          <div>Box 2</div>
+          <div>Box 3</div>
+        </div>
+      </div>
+    </div>
   )
 }
 
