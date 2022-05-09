818030b0e8906ca6c2a6f3623e5a82548d7e65ed
Matt Radich <mattradich24@gmail.com>
2022-05-09 22:55:29 +1200

    results-page-styling-done

--- client/components/SearchResults/SearchResults.jsx
+++ client/components/SearchResults/SearchResults.jsx
@@ -8,22 +8,31 @@ function UserResults({ user, title }) {
       <h2 className='user-name'>
         {user.firstName} {user.lastName}
       </h2>
-    <Link to={`../user/${user.id}`}>
-      <div>
-        <img className="results-profile-image" src="/defaultProfileImage.jpg" />
-        <h2>{user.firstName}</h2>
+    <Link id='link' to={`../user/${user.id}`}>
+      <div className='pfp-info'>
+        <div className='image-username-wrapper'>
+          <img className="results-profile-image" src="/defaultProfileImage.jpg" />
+          <h2 id='user-name'>{user.firstName}</h2>
+        </div>
         <div>
           <>
-            <h4>{`I want to ${title}`}</h4>
-            <ul>
+            <h4 id='i-want-to'>{`I want to ${title}`}</h4>
+            <ul className='skill-list'>
               {user.skills.map((item) => {
                 return item.role == title && <li key={item.id}>{item.skill}</li>
               })}
             </ul>
           </>
         </div>
+        
       </div>
+      
+        
+          
+        
+    
     </Link>
+    
     </div>
     </div>
   )
