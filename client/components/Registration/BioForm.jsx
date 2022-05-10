2813a437a1899b7de2f3b7a18d91aa9719124e6c
garrett-wong <your-email-address>
2022-05-10 14:29:10 +1200

    dev branch formatting complete
    Signed-off-by: garrett-wong <your-email-address>

--- client/components/Registration/BioForm.jsx
+++ client/components/Registration/BioForm.jsx
@@ -11,73 +11,73 @@ function BioForm({ setBio, bio }) {
   }
 
   return (
-    <div className='bioform-container'>
-      <div className='myprofile-container'>
-      <h2 id='profile-title'>My Profile</h2>
-    <div className='name-container'>
-      <label htmlFor="firstName" className="label">
-        First Name
-      </label>
-      <input
-        className="form-box"
-        id="firstName"
-        name="firstName"
-        onChange={handleChange}
-        value={bio?.firstName || ''}
-        required
-      />
+    <div className="bioform-container">
+      <div className="myprofile-container">
+        <h2 id="profile-title">My Profile</h2>
+        <div className="name-container">
+          <label htmlFor="firstName" className="label">
+            First Name
+          </label>
+          <input
+            className="form-box"
+            id="firstName"
+            name="firstName"
+            onChange={handleChange}
+            value={bio?.firstName || ''}
+            required
+          />
 
-      <label htmlFor="lastName" className="label">
-        Last Name
-      </label>
-      <input
-        className="form-box"
-        id="lastName"
-        name="lastName"
-        onChange={handleChange}
-        value={bio?.lastName || ''}
-      />
-      </div>
-      <div className='name-container'>
-      <label htmlFor="username" className="label">
-        Username
-      </label>
-      <input
-        className="form-box"
-        id="username"
-        name="username"
-        onChange={handleChange}
-        value={bio?.username || ''}
-        required
-      />
+          <label htmlFor="lastName" className="label">
+            Last Name
+          </label>
+          <input
+            className="form-box"
+            id="lastName"
+            name="lastName"
+            onChange={handleChange}
+            value={bio?.lastName || ''}
+          />
+        </div>
+        <div className="name-container">
+          <label htmlFor="username" className="label">
+            Username
+          </label>
+          <input
+            className="form-box"
+            id="username"
+            name="username"
+            onChange={handleChange}
+            value={bio?.username || ''}
+            required
+          />
 
-      <label htmlFor="email" className="label">
-        Email
-      </label>
-      <input
-        className="form-box"
-        name="email"
-        id="email"
-        value={bio?.email || ''}
-        onChange={handleChange}
-        disabled={true}
-      />
-  </div>
-  <div className='about-input-form'>
-    <label  htmlFor="about" className="label">
-        About
-      </label>
-      <textarea
-        className="form-box"
-        id="about"
-        name="about"
-        onChange={handleChange}
-        value={bio?.about || ''}
-        required
-      />
+          <label htmlFor="email" className="label">
+            Email
+          </label>
+          <input
+            className="form-box"
+            name="email"
+            id="email"
+            value={bio?.email || ''}
+            onChange={handleChange}
+            disabled={true}
+          />
+        </div>
+        <div className="about-input-form">
+          <label htmlFor="about" className="label">
+            About
+          </label>
+          <textarea
+            className="form-box"
+            id="about"
+            name="about"
+            onChange={handleChange}
+            value={bio?.about || ''}
+            required
+          />
+        </div>
       </div>
     </div>
-  </div>
   )
 }
 
--- client/pages/Profile.jsx
+++ client/pages/Profile.jsx
@@ -17,54 +17,58 @@ function Profile() {
 
   if (waiting) {
     return <WaitIndicator />
-  } else if (error) { // we want to show this if user not found
+  } else if (error) {
+    // we want to show this if user not found
     return <Error message={error} />
   } else if (!user.username) {
     return <div>No users found</div>
   } else {
-    
     const teach = user.skills.filter((x) => x.role === 'teach')
     const learn = user.skills.filter((x) => x.role === 'learn')
     return (
       <div className="page-container">
-      <h2 id='teachers-profile'>Profile</h2>
-      <div className="profile-container">
-        <div className="profile-column">
-          <div className="profile-card">
-            <div className="profile-welcome">My name is {user.firstName}</div>
-            <img id='user-pfp' src={user.picture || '/defaultProfileImage.jpg'} alt={user.firstName} />
-            <div className="profile-about">
-              Learn about me:<br></br>
-              <br></br>
-              {user.about}
+        <h2 id="teachers-profile">Profile</h2>
+        <div className="profile-container">
+          <div className="profile-column">
+            <div className="profile-card">
+              <div className="profile-welcome">My name is {user.firstName}</div>
+              <img
+                id="user-pfp"
+                src={user.picture || '/defaultProfileImage.jpg'}
+                alt={user.firstName}
+              />
+              <div className="profile-about">
+                Learn about me:<br></br>
+                <br></br>
+                {user.about}
+              </div>
+              <div className="profile-card">
+                Connect with me directly:<br></br>
+                <br></br>
+                {user.email}
+              </div>
+            </div>
+          </div>
+          <div className="profile-column">
+            <div className="profile-card">
+              <h4>Things I can TEACH you:</h4>
+              {teach.map((x) => (
+                <div className="profile-skill" key={x.id}>
+                  {x.skill}
+                </div>
+              ))}
             </div>
             <div className="profile-card">
-              Connect with me directly:<br></br>
-              <br></br>
-              {user.email}
+              <h4>I want to LEARN:</h4>
+              {learn.map((x) => (
+                <div className="profile-skill" key={x.id}>
+                  {x.skill}
+                </div>
+              ))}
             </div>
-          </div>
-        </div>
-        <div className="profile-column">
-          <div className="profile-card">
-            <h4>Things I can TEACH you:</h4>
-            {teach.map((x) => (
-              <div className="profile-skill" key={x.id}>
-                {x.skill}
-              </div>
-            ))}
-          </div>
-          <div className="profile-card">
-            <h4>I want to LEARN:</h4>
-            {learn.map((x) => (
-              <div className="profile-skill" key={x.id}>
-                {x.skill}
-              </div>
-            ))}
           </div>
         </div>
       </div>
-    </div>
     )
   }
 }
