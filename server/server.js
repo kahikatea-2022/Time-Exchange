2d91825290aa2332eafce45df1636b6678143667
HobbesWSL <jessewilson@outlook.com>
2022-05-10 21:04:39 +1200

    set up socket server and listenr

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
 // Imports
 const express = require('express')
+const socketIo = require('socket.io')
+const http = require('http')
 const path = require('path')
+
+// db functions
+const { getConversationId } = require('./db/conversations')
+const { getMessagesByConversation, addMessage } = require('./db/messages')
 
 // API Routes
 const usersRoutes = require('./routes/users')
@@ -9,7 +15,9 @@ const categoryRoutes = require('./routes/categories')
 const regionRoutes = require('./routes/regions')
 
 //Setup
-const server = express()
+const app = express()
+const server = http.createServer(app)
+const io = socketIo(server)
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
@@ -19,6 +27,40 @@ server.use('/api/v1/users', usersRoutes)
 server.use('/api/v1/user', userRoutes)
 server.use('/api/v1/categories', categoryRoutes)
 server.use('/api/v1/regions', regionRoutes)
+
+//Socket events
+io.on('connection', (socket) => {
+  socket.on('join_conversation', async ({ user_1, user_2 }) => {
+    const conversationID = await getConversationId(user_1, user_2)
+    const history = await getMessagesByConversation(conversationID)
+    socket.join(conversationID)
+    socket.emit('conversation', { conversationID, history })
+    // add try catch!!! with error emitter
+  })
+
+  socket.on('new_message', async (message) => {
+    try {
+      await addMessage({
+        conversation_id: message.conversationID,
+        user_id: message.userId,
+        message: message.message,
+        uuid: message.uuid,
+      })
+      io.to(message.conversationID).emit('new_message', message)
+    } catch (error) {
+      console.log(error)
+      // emit error message
+    }
+  })
+
+  socket.on('close_conversation', (conversationID) => {
+    socket.leave(conversationID)
+  })
+
+  socket.on('disconnect', () => {
+    console.log('disconnected')
+  })
+})
 
 // React APP endpoint
 server.get('*', (req, res) => {
