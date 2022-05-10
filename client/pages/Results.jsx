b58d8f7519145c31c26fee4996c2c5d13facc3b3
Jasmine Sinclair <jasminersinclair@gmail.com>
2022-05-10 18:41:51 +1200

    ran formatter on all files

--- client/pages/Results.jsx
+++ client/pages/Results.jsx
@@ -46,33 +46,33 @@ function Results() {
     return <Error message={error} />
   } else {
     return (
-      <div className='results-container'>
-        <div className='type-heading-container'>
-        
-            <h1 className='results-title'>{`Here are your ${type}`}</h1>
+      <div className="results-container">
+        <div className="type-heading-container">
+          <h1 className="results-title">{`Here are your ${type}`}</h1>
         </div>
-        <div className='results-title-container'>
-          
-                {<input className='search-bar'
-                  type="search"
-                  placeholder="Search by Skill or Category.."
-                  value={search}
-                  onChange={onSearchChange}
-                />}
-                
+        <div className="results-title-container">
+          {
+            <input
+              className="search-bar"
+              type="search"
+              placeholder="Search by Skill or Category.."
+              value={search}
+              onChange={onSearchChange}
+            />
+          }
         </div>
-          
-        <div className='results'>
-                {filteredUsers.map((result) => {
-                return (
-                  <SearchResults
-                    user={result}
-                    resultsType={resultsType}
-                    title={role}
-                    key={result.id}
-                  />
-                )
-              })}
+
+        <div className="results">
+          {filteredUsers.map((result) => {
+            return (
+              <SearchResults
+                user={result}
+                resultsType={resultsType}
+                title={role}
+                key={result.id}
+              />
+            )
+          })}
         </div>
       </div>
     )
@@ -80,4 +80,3 @@ function Results() {
 }
 
 export default Results
- 
